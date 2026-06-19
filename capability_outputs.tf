b2958f5a18b8150b4f4c276e26bdcd6e130d4764
Brad Sickles <brad.sickles@gmail.com>
2026-06-19 09:00:44 -0400

    Upgraded capability scaffolding

---
+++ capability_outputs.tf
@@ -0,0 +1,15 @@
+locals {
+  // This indicates which outputs are supported by this app module
+  // When adding support for a new output, add it to this list; the output will be available at "local.capabilities.<output_name>"
+  capability_output_names = [
+    "env",
+    "secrets",
+    "private_urls",
+    "public_urls",
+    "log_configurations",
+    "mount_points",
+    "sidecars",
+    "events",
+    "metrics",
+  ]
+}
